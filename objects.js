71239c12f2488d4d3195e3ef22e36d1e66e3097d
Lalit sharma <Lsharma906850@gmail.com>
2025-01-08 16:31:26 +0530

    objects in js

--- objects.js
+++ objects.js
@@ -1,14 +1,28 @@
-function Book(title,author,pages,isRead){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
+// function Book(title,author,pages,isRead){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.isRead = isRead;
 
-    this.info = function(){
-        return `${this.title}by ${this.author} ${this.pages}pages, ${this.isRead ? "read":  "not read yet"}`;
-    };
+//     this.info = function(){
+//         return `${this.title}by ${this.author} ${this.pages}pages, ${this.isRead ? "read":  "not read yet"}`;
+//     };
+// }
+
+// const theHobbit = new Book("The Hobbit","JRR",295,false);
+
+// console.log(theHobbit.info());
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+const obj3 = Object.assign(obj1,obj2)
+// console.log(obj3)
+
+const course = {
+    coursename : "js in hindi",
+    coursePrice : "999",
+    courseInstructor : "Lalit"
 }
 
-const theHobbit = new Book("The Hobbit","JRR",295,false);
-
-console.log(theHobbit.info());
+const {courseInstructor} = course
+console.log(courseInstructor)
